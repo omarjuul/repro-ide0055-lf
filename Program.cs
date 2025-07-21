9325b37592245fa0e55dedaac1e1e3f159b74ddc
Camiel Egbers <camiel.egbers@ns.nl>
2025-07-21 12:29:05 +0200

    add repro

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿#pragma warning disable CS8321 // Local function is declared but never used
+
+static int GetTheValueWithComment() =>
+    // A comment
+    42;
+
+static int GetTheValueWithCommentAlternative()
+    // A comment
+    => 42;
+
+static int GetTheValue() => 42;
+
+static int GetTheValueNewlineFirst()
+    => 42;
+
+static int GetTheValueNewlineAfter() =>
+    42;
